c604037169c06843f4a0bf7fbe92a206a5ebdf97
Fauzan Akmal Mahdi <recovery252@gmail.com>
2022-12-19 23:02:12 +0700

    set mpa web definition

--- lib/src/config/responsive_mpa_web.dart
+++ lib/src/config/responsive_mpa_web.dart
@@ -3,6 +3,59 @@ import 'package:responsive_mpa_web/responsive_mpa_web.dart';
 import 'package:responsive_mpa_web/src/responsive_mpa_web.dart';
 import 'package:responsive_mpa_web/src/appbar/appbar_menu_button.dart';
 
+///Main configuration of `responsive_mpa_web` plugin to set 
+///`listMenu` in [AppBarMenu]
+/// 
+/// example:
+/// 
+/// ```dart
+/// return MaterialApp(
+///  title: 'Flutter Demo',
+///  home: ResponsiveMPAWeb(
+///    listMenu: [
+///      AppBarMenuButton(
+///        menuText: const Text("Menu 1"),
+///        indexPage: 1,
+///        onTap: (context) {
+///          Navigator.push(
+///            context,
+///            FadeInRoute(
+///              page: const HomePage(),
+///              routeName: '/home',
+///            ),
+///          );
+///        },
+///      ),
+///      AppBarMenuButton(
+///        menuText: const Text("Menu 2"),
+///        indexPage: 2,
+///        onTap: (context) {
+///          Navigator.push(
+///            context,
+///            FadeInRoute(
+///              page: const SecondPage(),
+///              routeName: '/secondpage',
+///            ),
+///          );
+///        },
+///      ),
+///      AppBarMenuButton(
+///        menuText: const Text("Menu 3"),
+///        indexPage: 3,
+///        onTap: (context) {
+///          Navigator.push(
+///            context,
+///            FadeInRoute(
+///              page: const ThirdPage(),
+///              routeName: '/thirdpage',
+///            ),
+///          );
+///        },
+///      ),
+///    ],
+///    child: const HomePage(),
+///  ),
+/// ```
 class ResponsiveMPAWeb extends StatelessWidget {
   final List<AppBarMenuButton> listMenu;
   final Widget child;
