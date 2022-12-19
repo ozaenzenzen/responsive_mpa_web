7d862814ad91c1ef616c721f3e8ee21b1fa98e35
Fauzan Akmal Mahdi <recovery252@gmail.com>
2022-12-19 23:17:11 +0700

    update example

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:responsive_mpa_web_example/app.dart';
+import 'package:responsive_mpa_web_example/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
