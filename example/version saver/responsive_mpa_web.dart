0ba74b6cfbd5253276a1a7b597234dc8ed1e11e7
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-01-05 00:28:22 +0700

    update version saver

---
+++ example/version saver/responsive_mpa_web.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_mpa_web/responsive_mpa_web.dart';
+
+///Main configuration of `responsive_mpa_web` plugin to set 
+///`listMenu` in [AppBarMenu]
+/// 
+/// example:
+/// 
+/// ```dart
+/// return MaterialApp(
+///  title: 'Flutter Demo',
+///  home: ResponsiveMPAWeb(
+///    listMenu: [
+///      AppBarMenuButton(
+///        menuText: const Text("Menu 1"),
+///        indexPage: 1,
+///        onTap: (context) {
+///          Navigator.push(
+///            context,
+///            FadeInRoute(
+///              page: const HomePage(),
+///              routeName: '/home',
+///            ),
+///          );
+///        },
+///      ),
+///      AppBarMenuButton(
+///        menuText: const Text("Menu 2"),
+///        indexPage: 2,
+///        onTap: (context) {
+///          Navigator.push(
+///            context,
+///            FadeInRoute(
+///              page: const SecondPage(),
+///              routeName: '/secondpage',
+///            ),
+///          );
+///        },
+///      ),
+///      AppBarMenuButton(
+///        menuText: const Text("Menu 3"),
+///        indexPage: 3,
+///        onTap: (context) {
+///          Navigator.push(
+///            context,
+///            FadeInRoute(
+///              page: const ThirdPage(),
+///              routeName: '/thirdpage',
+///            ),
+///          );
+///        },
+///      ),
+///    ],
+///    child: const HomePage(),
+///  ),
+/// ```
+class ResponsiveMPAWeb extends StatelessWidget {
+  final List<AppBarMenuButton> listMenu;
+  final Widget child;
+
+  const ResponsiveMPAWeb({
+    Key? key,
+    required this.listMenu,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ResponsiveMPAWebConfig.listMenu = listMenu;
+    return child;
+  }
+}
